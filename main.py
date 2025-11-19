607bde4fa9c186af5f4dfa044b503b5528545b75
MatejSulic <sul.matej@gmail.com>
2025-11-19 23:21:15 +0100

    color and draw editing

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from drawer.matplotlib_drawer import Drawer
 def main():
     # 1) načtení tvarů ze souboru
     shapes = load_geometry("shapes_clean.txt")
-    shape = shapes[list(shapes.keys())[0]]
+    shape = shapes[list(shapes.keys())[2]]
 
 
     centroid = symmetric_x_centroid(shape)
@@ -20,15 +20,15 @@ def main():
     result = (
         shape
         .pipe(mirror_x)
-        .pipe(lambda s: add_radial_noise(s, max_damage=0.2))
+        .pipe(lambda s: add_radial_noise(s, max_damage=0.08))
     )
 
     # 3) vykreslení
     d = Drawer()
-    d.draw_shape(shape, color="blue", linewidth=1)  # původní tvar
-    d.draw_shape(shape_before_transform, color="gray", linewidth=1) # před zásahem šumu    
+    d.draw_shape(shape, color="blue", linewidth=2)  # původní tvar
+    d.draw_shape(shape_before_transform, color="gray", linewidth=0.25) # před zásahem šumu    
     d.draw_points(point_centroid, color="red", size=50)  # těžiště    
-    d.draw_shape(result, color="black")     # deformovaný
+    d.draw_shape(result, color="black", linewidth=2)     # deformovaný
     d.show()
 
 
